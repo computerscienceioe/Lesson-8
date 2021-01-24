079f1f6892141e75a9c55311feeb5c798837150b
MrBenjaminCounter <77814190+MrBenjaminCounter@users.noreply.github.com>
2021-01-24 21:46:01 +0000

    Create Challenge 2.py

---
+++ Challenge 2.py
@@ -0,0 +1,10 @@
+
+subject = "Computer Sciences"
+subject = subject.upper()
+print(len(subject))
+
+partOne = subject[:len(subject)//2]
+partTwo = subject[len(subject)//2:]
+
+print(partOne)
+print(partTwo)
